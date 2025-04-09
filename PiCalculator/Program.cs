fb2fc92e3255f708a802348afbf1c910fcac1bb8
Yerbol <zh_zh_1976@mail.ru>
2025-04-09 10:16:14 +0500

    add diagnostic info

--- PiCalculator/Program.cs
+++ PiCalculator/Program.cs
@@ -1,4 +1,6 @@
-﻿[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UnitTest")]
+﻿using System.Diagnostics;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UnitTest")]
 
 namespace PiComputation;
 
@@ -6,32 +8,26 @@ internal static class PiCalculator
 {
     private const long NUM_STEPS = 200000000;
 
-    internal static double ComputePiSequential()
-    {
-        double sum = 0.0;
-        for (long i = 0; i < NUM_STEPS; i++)
-        {
-            sum += 1.0 / (4 * i + 1) - 1.0 / (4 * i + 3);
-        }
-        return sum * 4.0;
-    }
-
     internal static double ComputePiParallel(int numThreads)
     {
         double[] partialSums = new double[numThreads];
         Thread[] threads = new Thread[numThreads];
 
+        long chunk = NUM_STEPS / numThreads;
+
         for (int tid = 0; tid < numThreads; tid++)
         {
-            int threadId = tid; 
+            long start = tid * chunk;
+            long end = (tid == numThreads - 1) ? NUM_STEPS : start + chunk;
+            int threadIndex = tid;
             threads[tid] = new Thread(() =>
             {
                 double sum = 0.0;
-                for (long i = threadId; i < NUM_STEPS; i += numThreads)
+                for (long i = start; i < end; i ++)
                 {
                     sum += 1.0 / (4 * i + 1) - 1.0 / (4 * i + 3);
                 }
-                partialSums[threadId] = sum;
+                partialSums[threadIndex] = sum;
             });
             threads[tid].Start();
         }
@@ -54,8 +50,14 @@ internal static class Program
 {
     internal static void Main(string[] args)
     {
-        Console.WriteLine("Введите число:");
-        string input = Console.ReadLine();
+        Console.WriteLine("Введите число потоков или слово test для диагностики");
+        string? input = Console.ReadLine().Trim();
+        
+        if (input.ToLower() == "test")
+        {
+            RunAutomaticMeasurements();
+            return;
+        }
 
         bool isParsed = int.TryParse(input, out int numThreads);
         if (!isParsed || numThreads <= 0)
@@ -65,8 +67,37 @@ internal static class Program
             return;
         }
 
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
         double pi = PiCalculator.ComputePiParallel(numThreads);
+        stopwatch.Stop();
+
         Console.WriteLine("pi = {0:F15}", pi);
         Console.ReadLine();
+    }
+
+    private static void RunAutomaticMeasurements()
+    {
+        int maxThreads = 100;
+        string fileName = "DiagnosticInfo.csv";
+
+        using (StreamWriter swFile = new StreamWriter(fileName))
+        {
+            swFile.WriteLine("Threads, ElapsedMilliseconds, PiValue");
+
+            for (int threads = 1; threads <= maxThreads; threads++)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                double pi = PiCalculator.ComputePiParallel(threads);
+                stopwatch.Stop();
+
+                swFile.WriteLine($"{threads},{stopwatch.ElapsedMilliseconds},{pi:F15}");
+                Console.WriteLine($"Потоков: {threads}, Время: {stopwatch.ElapsedMilliseconds} ms, pi = {pi: F15}"); 
+            }
+        }
+        
+    Console.WriteLine($"Готово{fileName}");
+    Console.ReadLine();
     }
 }
